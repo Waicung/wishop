55e8df7a0d005bca9b7708d3b723c58187b3b87f
Waicung <better@waicung.net>
2020-06-05 19:25:25 +0800

    promisifyAll on Launch

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //app.js
-import { promisify } from "miniprogram-api-promise";
-const { getGetResponse } = require("/apis/index");
+import {
+  promisifyAll
+} from "miniprogram-api-promise";
+const {
+  getGetResponse
+} = require("/apis/index");
 const ENV = require("env");
+const wxp = {};
+promisifyAll(wx, wxp);
 
 App({
   onLaunch: function () {
@@ -11,15 +17,17 @@ App({
     wx.setStorageSync("logs", logs);
 
     // 登录
-    promisify(wx.login)()
+    wxp
+      .login()
       .then((res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         if (res.code) {
           getGetResponse(ENV.REMOTE_SERVER, "login", {
-            code: res.code,
-          })
+              code: res.code,
+            })
             .then((res) => {
               if (res.statusCode === 200) {
+                console.log("登陆成功");
                 //TODO 登录态管理
                 //console.log(res.data.sessionId); // 服务器回包内容
               }
@@ -34,28 +42,25 @@ App({
       });
 
     // 获取用户信息
-    wx.getSetting({
-      success: (res) => {
-        if (res.authSetting["scope.userInfo"]) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: (res) => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo;
+    wxp.getSetting().then((res) => {
+      if (res.authSetting["scope.userInfo"]) {
+        // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        wxp.getUserInfo().then((res) => {
+          // 可以将 res 发送给后台解码出 unionId
+          this.globalData.userInfo = res.userInfo;
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res);
-              }
-            },
-          });
-        }
-      },
+          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          // 所以此处加入 callback 以防止这种情况
+          if (this.userInfoReadyCallback) {
+            this.userInfoReadyCallback(res);
+          }
+        });
+      }
     });
   },
   // 全局变量
   globalData: {
     userInfo: null,
+
   },
 });
